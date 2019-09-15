80ce09268a462705d0a5736759d74ea316fd79a4
Alistair Kane <alistairkane92@hotmail.co.uk>
2019-09-15 23:10:59 +0100

    lint and refactor cardType

--- client/src/stacked-group/cards/cardType.js
+++ client/src/stacked-group/cards/cardType.js
@@ -1,16 +1,16 @@
 const cardType = (title) => {
   const styles = {
-    Read: 'card-type-read',
-    Watch: 'card-type-watch',
-    Listen: 'card-type-listen',
+    read: 'card-type-read',
+    watch: 'card-type-watch',
+    listen: 'card-type-listen',
   };
 
   const container = document.createElement('div');
   const p = document.createElement('p');
   p.textContent = title.toUpperCase();
-  container.classList.add('card-type', styles[title]);
+  container.classList.add('card-type', styles[title.toLowerCase()]);
   container.appendChild(p);
   return container;
-}
+};
 
 export default cardType;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const config = {
-	entry: __dirname + "/client/src/app.js",
-	output: {
-		filename: "bundle.js",
-		path: __dirname + "/client/build"
-	},
-	devtool: "source-map"
+  entry: `${__dirname}/client/src/app.js`,
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/client/build`,
+  },
+  devtool: 'source-map',
 };
 
 module.exports = config;
